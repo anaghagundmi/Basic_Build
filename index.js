ae314a1d3b7549fb57d354bf4d724676c7312df5
anaghagundmi <81741028+anaghagundmi@users.noreply.github.com>
2021-07-24 15:45:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ stdin.addListener("data", function (d){
     let res;
     res = parseInt(d)%2 == 0 ? "even" : "odd"
 console.log('You entered: ', res)
-//return res
+//return res val
 process.exit(0)
 })
